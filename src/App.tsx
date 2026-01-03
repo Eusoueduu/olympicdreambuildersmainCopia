8a46fae2efec4e648af7e55d1a4bd8c66b96762e
Eusoueduu <p-eduardo2015@hotmail.com>
2026-01-03 17:02:35 -0300

    fix: index import case

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Index from "./pages/Index";
+import Index from "./pages/index"; // ✅ corrigido (minúsculo)
 import Sobre from "./pages/Sobre";
 import CartaConvite from "./pages/CartaConvite";
 import Fotos from "./pages/Fotos";
@@ -14,24 +14,24 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/sobre" element={<Sobre />} />
-          <Route path="/carta-convite" element={<CartaConvite />} />
-          <Route path="/fotos" element={<Fotos />} />
-          <Route path="/objetivos" element={<Objetivos />} />
-          <Route path="/contatos" element={<Contatos />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/sobre" element={<Sobre />} />
+            <Route path="/carta-convite" element={<CartaConvite />} />
+            <Route path="/fotos" element={<Fotos />} />
+            <Route path="/objetivos" element={<Objetivos />} />
+            <Route path="/contatos" element={<Contatos />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
